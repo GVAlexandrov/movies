7414c5de7bde9d47da5b35ab7b6e02f4bee88c82
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-09-24 00:03:01 +0300

    UPDATE App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
-import Main from './Components/Main/Main';
+import Home from './Components/Home/Home';
+import Movies from './Components/Movies/Movies';
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
+import MyMovies from './Components/Movies copy/MyMovies';
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      <Main />
+      <main>
+
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/movies' element={<Movies />} />
+          <Route path='/my-movies' element={<MyMovies />} />
+
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+
+      </main>
 
       <Footer />
     </div>
