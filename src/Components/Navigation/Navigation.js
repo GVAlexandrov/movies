22a0551db84a793352500c00393ae8891880fed9
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-10-01 01:23:10 +0300

    UPDATE Navigation.js

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,18 +1,35 @@
+import { logout } from '../../services/authService';
 import './Navigation.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
-const Navigation = () => {
+const Navigation = ({ userEmail, setUserEmail }) => {
+    const navigate = useNavigate();
+
+    const logoutHandler = (e) => {
+        e.preventDefault();
+        logout()();
+        setUserEmail(null);
+        localStorage.removeItem('uid');
+        localStorage.removeItem('email');
+        navigate('/home')
+    }
+
+
     return (
         <nav>
             <ul>
                 <li><NavLink to={'/home'}>Home</NavLink></li>
                 <li><NavLink to={'/movies'}>Movies</NavLink></li>
 
-                <li><NavLink to={'/register'}>Register</NavLink></li>
-                <li><NavLink to={'/login'}>Login</NavLink></li>
-
-                <li><NavLink to={'/my-movies'}>My movies</NavLink></li>
-                <li><NavLink to={'/'}>Logout</NavLink></li>
+                {userEmail !== null
+                    ? (<>
+                        <li><NavLink to={'/my-movies'}>My movies</NavLink></li>
+                        <li><NavLink onClick={logoutHandler} to={'/home'}>Logout</NavLink></li>
+                    </>)
+                    : (<>
+                        <li><NavLink to={'/register'}>Register</NavLink></li>
+                        <li><NavLink to={'/login'}>Login</NavLink></li>
+                    </>)}
             </ul>
         </nav>
     )
