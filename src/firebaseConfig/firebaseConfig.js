285bb69a655d848059b6e6981cf86e6d22be4850
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-09-30 20:29:34 +0300

    CREATE firebaseConfig.js

---
+++ src/firebaseConfig/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCuxpMcyvI2pqBQkpAUDytjNjoDOAoD9pI",
+    authDomain: "movies-ce449.firebaseapp.com",
+    projectId: "movies-ce449",
+    storageBucket: "movies-ce449.appspot.com",
+    messagingSenderId: "861839839140",
+    appId: "1:861839839140:web:63a265964b77f45fc7bb31"
+};
