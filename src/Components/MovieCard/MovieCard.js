26b8f840748a4296ed8d050252abfb4407b4ce59
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-10-08 19:38:20 +0300

    UPDATE MovieCard.js

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,11 +1,9 @@
 import './MovieCard.css';
 
 const MovieCard = (movieObj) => {
-    console.log(movieObj.movieObj.Title);
 
     return (
         <div className='div-movie-card'>
-            {/* <h4>{movieObj?.movieObj.Title}</h4> */}
             <img
                 alt={movieObj?.movieObj.Title}
                 src={movieObj?.movieObj.Poster}
