b2f79284449ce9b173d852ea4e636b2d2a4b4df7
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-10-08 18:17:16 +0300

    UPDATE Movies.js

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -1,16 +1,34 @@
 import './Movies.css';
 import MovieCard from '../MovieCard/MovieCard';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const API_URL = 'http://www.omdbapi.com?apikey=b02986ff';
 
 const Movies = () => {
+    let [movies, setMovies] = useState([]);
 
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const result = await response.json();
+        setMovies(result.Search);
+    }
+
+    movies.forEach(movieObj => {
+        console.log(movieObj);
+    });
+
+    useEffect(() => {
+        searchMovies('batman');
+    }, []);
 
     return (
         <div className='MoviesWrapper'>
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
+            {
+                movies?.map((movieObj) => {
+                    return <MovieCard movieObj={movieObj} />
+                })
+            }
         </div>
     )
 }
