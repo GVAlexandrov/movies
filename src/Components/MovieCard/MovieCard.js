1524e3580b63c99ae4de3b4ee56aaca6c25a2f6b
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-10-10 21:13:21 +0300

    UPDATE MovieCard

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,13 +1,16 @@
 import './MovieCard.css';
+import { NavLink } from 'react-router-dom';
 
 const MovieCard = (movieObj) => {
 
     return (
         <div className='div-movie-card'>
-            <img
-                alt={movieObj?.movieObj.Title}
-                src={movieObj?.movieObj.Poster}
-            />
+            <NavLink to={`/my-movies/movie/${movieObj.movieObj.imdbID}`}>
+                <img
+                    alt={movieObj?.movieObj.Title}
+                    src={movieObj?.movieObj.Poster}
+                />
+            </NavLink>
         </div>
     )
 }
