6d55f308d903633c2c2cf61feb7fbdc697562b62
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-10-11 00:41:46 +0300

    CREATE MoviePage.js

---
+++ src/Components/MoviePage/MoviePage.js
@@ -0,0 +1,47 @@
+import './MoviePage.css';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { API_URL } from '../../utils/utils';
+
+const MoviePage = () => {
+    const { id } = useParams();
+    const [movie, setMovie] = useState({})
+
+    const getMovie = async (id) => {
+        try {
+            const response = await fetch(`${API_URL}&i=${id}`);
+            const result = await response.json();
+            setMovie(result);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getMovie(id);
+    }, []);
+
+    console.log(movie);
+
+    return (
+        <div className='div-movie-page'>
+            <h2>{movie.Title}</h2>
+
+            <div className='div-movie-page-div-content'>
+                <img
+                    alt={movie.Title}
+                    src={movie.Poster}
+                />
+                <div className='div-movie-page-div-content-info'>
+                    <p>Year: {movie.Year}</p>
+                    <p>Genre: {movie.Genre}</p>
+                    <p>Plot: {movie.Plot}</p>
+                    <p>Director: {movie.Director}</p>
+                    <p>Actors: {movie.Actors}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MoviePage;
