b731724eded19f42f248b0bd35ad3940053e7f3d
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-10-22 22:15:27 +0300

    UPDATE Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,18 @@
 import './Header.css';
+import { NavLink } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
 
 const Header = ({ userEmail, setUserEmail }) => {
     return (
         <>
-            <header className='header'>MOVIES<strong>APP</strong>, Hello, {
-                userEmail !== null
-                    ? localStorage.getItem('email')
-                    : 'Guest'
-            }
-            </header>
+            <NavLink to={'/home'}>
+                <header className='header'>MOVIES<strong>APP</strong>, Hello, {
+                    userEmail !== null
+                        ? localStorage.getItem('email')
+                        : 'Guest'
+                }
+                </header>
+            </NavLink>
 
             <Navigation
                 userEmail={userEmail}
