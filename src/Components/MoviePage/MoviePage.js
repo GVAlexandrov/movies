1ead0150a2dffb5b59304a4919997e1ce6889fa7
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-10-24 00:23:43 +0300

    UPDATE MoviePage.js

--- src/Components/MoviePage/MoviePage.js
+++ src/Components/MoviePage/MoviePage.js
@@ -1,22 +1,37 @@
 import './MoviePage.css';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { API_URL, moveScreenToTop } from '../../utils/utils';
+import { API_URL } from '../../utils/utils';
 
 const MoviePage = () => {
     const { id } = useParams();
     const [movie, setMovie] = useState({});
-
-    moveScreenToTop();
+    const [rate, setRate] = useState(0);
+    const [liked, setLiked] = useState(false);
 
     const getMovie = async (id) => {
         try {
             const response = await fetch(`${API_URL}&i=${id}`);
             const result = await response.json();
-            console.log(result)
             setMovie(result);
         } catch (error) {
             console.log(error)
+        }
+    }
+
+    const rateMovie = function (e) {
+        e.preventDefault();
+
+        if (e.target.className === 'star-div' || e.target.className === 'star-div star-div-selected') {
+            setRate(Number(e.target.id));
+        }
+    }
+
+    const likeMovie = function (e) {
+        e.preventDefault();
+
+        if (e.target.className === 'far fa-heart' || e.target.className === 'far fa-heart liked') {
+            setLiked(!liked);
         }
     }
 
@@ -29,13 +44,16 @@ const MoviePage = () => {
         <div className='div-movie-page'>
             <h2>{movie.Title}</h2>
 
-            <div className="stars">
+            <div className="stars" onClick={rateMovie}>
                 <h3>Rate</h3>
 
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
-                    <div className='star-div' key={value}>★</div>
-                ))}
-                <div>10 / 10</div>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+                    .map((value) => (
+                        value <= rate
+                            ? <div id={value} className='star-div star-div-selected' key={value}>★</div>
+                            : <div id={value} className='star-div' key={value}>★</div>
+                    ))}
+                <div>{rate} / 10</div>
             </div>
 
             <div className='div-movie-page-div-content'>
@@ -57,8 +75,16 @@ const MoviePage = () => {
             <div className="like-button">
                 <h3>Like</h3>
 
-                <span id={id} className="heart">
-                    <i className="far fa-heart"></i>
+                <span
+                    id={id}
+                    className="heart"
+                    onClick={likeMovie}
+                >
+                    <i className={
+                        liked
+                            ? "far fa-heart liked"
+                            : "far fa-heart"
+                    }></i>
                 </span>
             </div>
         </div>
