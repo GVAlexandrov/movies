fee3233ce9488a49ee46aeb2a196ec9dfcc1e9a4
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-10-25 00:14:13 +0300

    UPDATE MyMovies.js

--- src/Components/MyMovies/MyMovies.js
+++ src/Components/MyMovies/MyMovies.js
@@ -1,6 +1,20 @@
 import './MyMovies.css';
+import { getAllFavouriteMovies } from '../../services/services';
+import { useEffect, useState } from 'react';
+
 
 const MyMovies = () => {
+    let [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        getAllFavouriteMovies('movies')
+            .then(res => {
+                setMovies(res);
+            })
+    }, []);
+
+    console.log(movies);
+
     return (
         <>
             <h1>MY MOVIES</h1>
